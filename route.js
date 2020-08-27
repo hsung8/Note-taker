a3c76b7f0e9b0ca09d994a27966b5f1d65245165
hsung8 <hsung@atlantaregional.org>
2020-08-27 17:58:19 -0400

    wip

--- route.js
+++ route.js
@@ -10,10 +10,10 @@ const path = require('path');
 // ===============================================================================
 // ROUTING
 // ===============================================================================
-module.exports = function(app) {
+module.exports = function (app) {
 
     // Setup notes variable
-    fs.readFile("db/db.json","utf8", (err, data) => {
+    fs.readFile("db/db.json", "utf8", (err, data) => {
 
         if (err) throw err;
 
@@ -25,37 +25,51 @@ module.exports = function(app) {
         // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
         // ---------------------------------------------------------------------------
 
-        app.get("/api/notes", function(req, res) {
+        app.get("/api/notes", function (req, res) {
             res.json(notes);
         });
 
         // post route
-        app.post("/api/notes", function(req, res) {
+        app.post("/api/notes", function (req, res) {
             let newNote = req.body;
+            newNote.id = Date.now();
             notes.push(newNote);
             updateDb();
             //return console.log(`Added new note: ${newNote.title}`);
         });
 
         // get the note using id
-        app.get("/api/notes/:id", function(req,res) {
+        app.get("/api/notes/:id", function (req, res) {
             res.json(notes[req.params.id]);
         });
 
         // remove note by id
-        app.remove("/api/notes/:id", function(req, res) {
+        app.delete("/api/notes/:id", function (req, res) {
             notes.splice(req.params.id, 1);
             updateDb();
-            //console.log("Removed note with id "+req.params.id);
         });
 
         //updates after note add or note removal
         function updateDb() {
-            fs.writeFile("db/db.json",JSON.stringify(notes,'\t'),err => {
+            fs.writeFile("db/db.json", JSON.stringify(notes, '\t'), err => {
                 if (err) throw err;
                 return true;
             });
         }
+
+        // HTML GET Requests
+        // Below code handles when users "visit" a page.
+        // In each of the below cases the user is shown an HTML page of content
+        // ---------------------------------------------------------------------------
+        app.get('/notes', function (req, res) {
+            res.sendFile(path.join(__dirname, "public/notes.html"));
+        });
+
+        // link to index.html
+        app.get('*', function (req, res) {
+            res.sendFile(path.join(__dirname, "public/index.html"));
+        });
+
     });
 
 }
--- server.js
+++ server.js
@@ -3,17 +3,20 @@ const express = require("express");
 const fs = require("fs");
 const path = require('path');
 
-//require route
-require('./apiRoutes')(app);
-require('./htmlRoutes')(app);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+
 
 //set up express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname));
+
+
+//require route
+require('./route')(app);
 
 //listener
 app.listen(PORT, function() {
