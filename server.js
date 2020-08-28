54676e55aa52e54e6b397925be6230008c0f36c9
hsung8 <hsung@atlantaregional.org>
2020-08-27 22:38:53 -0400

    cleaning code

--- server.js
+++ server.js
@@ -2,21 +2,16 @@
 const express = require("express");
 const fs = require("fs");
 const path = require('path');
-
-
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-
+//require route
+require('./route')(app);
 
 //set up express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-
-
-//require route
-require('./route')(app);
 
 //listener
 app.listen(PORT, function() {
